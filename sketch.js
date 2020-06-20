b31f44bf16cef38d2834e234c07dba00a2e5e211
James-Lang <James1o9lang92@yahoo.com>
2020-06-19 22:13:54 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ function preload() {
 
 function setup() {
   // shipey = NeuralNetwork.deserialize(brainJSON);
-createCanvas(400, 400);
+createCanvas(100, 100);
 for (let i = 0; i < totalShips; i++){
 // let ship = new Ship(shipey);
 let ship = new Ship();
